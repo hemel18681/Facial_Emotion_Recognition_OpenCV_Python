ab2eb468c613d7a066f8935430a77cba9a858d91
hemel18681 <hemel18103112@gmail.com>
2020-10-24 00:35:25 +0600

    work more

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ while True:
     success, frame = cam.read()
     frame = er.recognise_emotion(frame,return_type = 'BGR')
     cv2.imshow('frame',frame)
-
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
 cam.release()
 cv2.destroyAllWindows()
